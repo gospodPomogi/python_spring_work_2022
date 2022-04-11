6e127bf381a0700cc4bdffdfc0a94d49e25173c9
Vadim Polikanov <lastresortforcake@gmail.com>
2022-04-11 18:43:17 +0300

    task6

--- python_spring_work_2022/task6.py
+++ python_spring_work_2022/task6.py
@@ -3,6 +3,6 @@ while True:
     if coefficient_a == 0:
         print('coefficient a cannot be equal to 0')
     else:
-        x = coefficient_b / coefficient_a
-        decision = coefficient_a * x - coefficient_b
+        x = -coefficient_b / coefficient_a
+        decision = coefficient_a * x + coefficient_b
         print(decision)
